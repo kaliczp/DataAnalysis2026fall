b9c192ccc860363ae3f5e8d77d68ab5b36d7dc06
Péter Kalicz <kaliczp@gmail.com>
2026-09-23 17:36:58 +0200

    Some additional informations added as comments

--- Intro.R
+++ Intro.R
@@ -1,15 +1,18 @@
-## Use a function
+## Use the citation function from the intro text.
 citation()
 ## If you not write bracets the code of function will printed automaticly
 citation
 ## If you makes some sintax error (e.g. not close bracket or apostrophes)
-## a plus sing will appear instead prompt use Esc
+## a plus sing will appear instead the normal prompt '>', then use Esc
+## if you can not solve the problem.
 citation(
-## Use question mark to acces manual
+## Use question mark to acces the user manual
 ?citation
 ## or alternatively help function. See the structure of manual!
+## See also the Examples section at the end!
 help(citation)
 ## Use help.start function to access help system.
+## You can access all installed package's documentation and generic manuals.
 help.start()
 ## Find directly vignettes for a package.
 vignette(package = "sf")
