2862b58aeabfac8cce16ff9a195264f129463796
Péter Kalicz <kaliczp@gmail.com>
2026-09-23 15:42:06 +0200

    Initial commit

---
+++ Intro.R
@@ -0,0 +1,17 @@
+## Use a function
+citation()
+## If you not write bracets the code of function will printed automaticly
+citation
+## If you makes some sintax error (e.g. not close bracket or apostrophes)
+## a plus sing will appear instead prompt use Esc
+citation(
+## Use question mark to acces manual
+?citation
+## or alternatively help function. See the structure of manual!
+help(citation)
+## Use help.start function to access help system.
+help.start()
+## Find directly vignettes for a package.
+vignette(package = "sf")
+## You can close the software from the promt too.
+q()
